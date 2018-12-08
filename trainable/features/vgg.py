6f765150de705ec0a9197a5bccd28a62c51e0f7e
Jeff Hilton <jeffhilton.code@gmail.com>
2018-12-08 02:12:38 -0700

    0.1.3.post5: Added to() to AvgPoolVGG class.

--- trainable/features/vgg.py
+++ trainable/features/vgg.py
@@ -11,6 +11,10 @@ class AvgPoolVGG(nn.Module):
             if isinstance(m, nn.MaxPool2d):
                 m = nn.AvgPool2d(kernel_size=2, stride=2)
 
+    def to(self, device):
+        self.vgg = self.vgg.to(device)
+        return self
+
     def children(self):
         return self.vgg.children()
 
