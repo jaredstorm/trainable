2653220517bf56c01376b647232005ed06074342
Jeff Hilton <jeffhilton.code@gmail.com>
2018-12-09 00:03:48 -0700

    Documentation update.

--- trainable/epoch.py
+++ trainable/epoch.py
@@ -6,23 +6,48 @@ class TrainingEpoch(object):
 
     Usually an epoch just consists of running data
     through the model in batches. But just in case,
-    this offers a bit more flexibility.
+    this base class offers a bit more flexibility.
+
+    Extending this Class:
+    To subclass a TrainingEpoch that works fluidly with
+    the rest of Trainable, it should:
+        1. override the __call__ method
+        2. The __call__ method should take at least the
+           following parameters:
+            a. A session instance
+            b. A DataLoader or some other data source
+            c. A target device for your data
+        3. The __call__ method should at very least:
+            a. make a call to the training algorithm, and store
+               the results.
+            b. call optim.zero_grad() and optim.step() for each
+               batch trained on. Algorithms purely run data through
+               a model, compute the desired loss, and compute the
+               gradient. Surrounding your call to the training algorithm
+               with these methods is not only good practice, but mandatory
+               for training to work properly.
+            c. call visualize.
+            d. call update on the loop.
+
+    Example::
+    >>> class DefaultTrainingEpoch(TrainingEpoch):
+    >>>     def __call__(self, session, data, device):
+    >>>         model, optim = session.model, session.optim
+    >>>         for batch in data:
+    >>>             optim.zero_grad()
+    >>>             metrics = self.train(model, batch, device)
+    >>>             optim.step()
+    >>>
+    >>>             session.append_metrics(metrics)
+    >>>             self.visualize(session.model, batch, device)
+    >>>             self.loop.update(session.epoch, metrics)
     """
 
-    def __init__(self):
+    def __init__(self, algorithm=None, loop=None, visualize=None):
         super().__init__()
-        self.__dict__.update(locals())
-
-    def __call__(self, session, data, device):
-        raise NotImplementedError
-
-
-class DefaultTrainingEpoch(TrainingEpoch):
-    def __init__(self):
-        super().__init__()
-        self.loop = None
-        self.train = None
-        self.visualize = None
+        self.train = algorithm
+        self.loop = loop
+        self.visualize = visualize
 
     def set_algorithm(self, algorithm):
         self.train = algorithm
@@ -36,6 +61,21 @@ class DefaultTrainingEpoch(TrainingEpoch):
     def get_visualizer(self):
         return self.visualize
 
+    def set_loop(self, loop):
+        self.loop = loop
+
+    def get_loop(self):
+        return self.loop
+
+    def __call__(self, session, data, device):
+        raise NotImplementedError
+
+
+class DefaultTrainingEpoch(TrainingEpoch):
+    """DefaultTrainingEpoch()
+    The default behavior for an epoch of training.
+    """
+
     def __call__(self, session, data, device):
         model, optim = session.model, session.optim
         for batch in data:
@@ -48,5 +88,5 @@ class DefaultTrainingEpoch(TrainingEpoch):
             self.loop.update(session.epoch, metrics)
 
 
-def test_default_epoch():
+def epoch_test():
     test.title("Training Epoch")
