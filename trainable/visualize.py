3a24619296a0a633baa6bdb30dc4c1a102fb7dbb
Jeff Hilton <jeffhilton.code@gmail.com>
2019-01-09 21:25:21 -0700

    0.1.4.dev20: Adjusting normalization defaults.

--- trainable/visualize.py
+++ trainable/visualize.py
@@ -1,3 +1,4 @@
+import torch
 from torchvision.utils import make_grid, save_image
 import matplotlib.pyplot as plt
 
@@ -7,7 +8,8 @@ import shutil
 ########################################################################################################################
 # Things to do                                                                                                         #
 ########################################################################################################################
-# TODO: Flesh out class and method descriptions
+# TODO: Flesh out class/method descriptions
+# TODO: Make normalization more sensible for Plotter and Saver
 
 class Visualizer(object):
     def __init__(self, frequency=1, batch=True, padding=0):
@@ -67,7 +69,6 @@ class Plotter(Visualizer):
                 row,
                 self.padding,
                 normalize=True,
-                scale_each=True,
                 pad_value=255
             )
 
@@ -77,7 +78,7 @@ class Plotter(Visualizer):
 
             model.train()
 
-            return axis
+            return True
 
     def visualize_single(self, model, batch, device):
         with torch.no_grad():
@@ -91,7 +92,7 @@ class Plotter(Visualizer):
             plt.show()
             model.train()
 
-            return axis
+            return True
 
 
 class Saver(Visualizer):
@@ -121,12 +122,13 @@ class Saver(Visualizer):
                 row,
                 self.padding,
                 normalize=True,
-                scale_each=True,
             )
 
             self.samples += 1
 
             model.train()
+
+            return True
 
     def visualize_single(self, model, batch, device):
         with torch.no_grad():
@@ -143,6 +145,8 @@ class Saver(Visualizer):
 
             model.train()
 
+            return True
+
 
 class PlotterSaver(Visualizer):
     def __init__(self, image_folder, frequency=1, batch=True, padding=0):
@@ -151,89 +155,13 @@ class PlotterSaver(Visualizer):
         self.saver = Saver(image_folder, frequency, batch, padding)
 
     def visualize_batch(self, model, batch, device):
-        self.plotter.visualize_batch(model, batch, device)
-        self.saver.visualize_batch(model, batch, device)
+        ret1 = self.plotter.visualize_batch(model, batch, device)
+        ret2 = self.saver.visualize_batch(model, batch, device)
+        return ret1 and ret2
 
     def visualize_single(self, model, batch, device):
-        self.plotter.visualize_single(model, batch, device)
-        self.saver.visualize_single(model, batch, device)
+        ret1 = self.plotter.visualize_single(model, batch, device)
+        ret2 = self.saver.visualize_single(model, batch, device)
+        return ret1 and ret2
 
-
-def plotter_test():
-    title("plotter")
-
-    model = torch.nn.Conv2d(3, 3, kernel_size=3, padding=1)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    batch = torch.randn(16, 3, 16, 16)
-
-    subtest(1, "Batch Functionality")
-    plot = Plotter()
-    plot(model, batch, device)
-
-    subtest(2, "Batch With Padding")
-    plot = Plotter(padding=2)
-    plot(model, batch, device)
-
-    subtest(3, "Single")
-    plot = Plotter(batch=False)
-    plot(model, batch, device)
-
-    subtest(4, "Different Frequency")
-    plot = Plotter(frequency=3)
-    passed = True
-    passed = passed if not plot(model, batch, device) else False
-    passed = passed if not plot(model, batch, device) else False
-    evaluate(passed)
-    plot(model, batch, device)
-
-    end()
-
-
-def saver_test():
-    if os.path.exists('./saver_tests'):
-        shutil.rmtree('./saver_tests')
-
-    def show(path):
-        plt.axis('off')
-        plt.imshow(plt.imread(path))
-        plt.show()
-
-    title("saver")
-
-    model = torch.nn.Conv2d(3, 3, kernel_size=3, padding=1)
-    device = torch.device('cuda'  if torch.cuda.is_available() else 'cpu')
-    batch = torch.randn(16, 3, 16, 16)
-
-    subtest(1, "Batch Functionality")
-    save = Saver("./saver_tests")
-    save(model, batch, device)
-    passed = True
-    passed = passed if save.samples == 1 else False
-    passed = passed if len(os.listdir('./saver_tests')) == save.samples else False
-    evaluate(passed)
-    show('./saver_tests/sample-0.png')
-
-    subtest(2, "Proper Sample Counting")
-    passed = True
-    save = Saver("./saver_tests")
-    passed = passed if save.samples == 1 else False
-    save(model, batch, device)
-    passed = passed if save.samples == 2 else False
-    passed = passed if len(os.listdir('./saver_tests')) == save.samples else False
-    evaluate(passed)
-    show('./saver_tests/sample-1.png')
-
-    subtest(3, "Padded Batches")
-    save = Saver("saver_tests", padding=2)
-    save(model, batch, device)
-    passed = True if len(os.listdir('./saver_tests')) == save.samples else False
-    evaluate(passed)
-    show('saver_tests/sample-2.png')
-
-    end()
-    shutil.rmtree('./saver_tests')
-
-
-if __name__ == '__main__':
-    saver_test()
 
