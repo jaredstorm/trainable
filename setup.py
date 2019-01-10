fb070f25f77bc97a4f6ad9edc36372b136e306c7
Jeff Hilton <jeffhilton.code@gmail.com>
2019-01-09 21:17:09 -0700

    0.1.4.dev20: THE TESTING PHASE.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='trainable',
-    version='0.1.4.dev19',
+    version='0.1.4.dev20',
     description='The flexible training toolbox',
     long_description=long_description,
     long_description_content_type='text/markdown',
